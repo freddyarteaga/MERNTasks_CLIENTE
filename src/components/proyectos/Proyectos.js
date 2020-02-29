f9955a9a03dd2f47b537445c2a725c849c3be72b
Freddy <freddy.artea@gmail.com>
2020-02-29 12:12:35 -0500

    creando el formulario para crear tareas

--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sidebar from '../layout/Sidebar'
 import Barra from '../layout/Barra'
+import FormTarea from '../tareas/FormTarea'
 
 const Proyectos = () => {
     return ( 
@@ -11,7 +12,7 @@ const Proyectos = () => {
                 <Barra />
                 <main>
                     <div className='contenedor-tareas'>
-
+                        <FormTarea />
                     </div>
                 </main>
             </div>
