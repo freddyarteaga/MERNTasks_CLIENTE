d2e63589ebb30cc4a6a980ee5b7de72d5c829e58
Freddy <freddy.artea@gmail.com>
2020-02-29 11:48:29 -0500

    staste y funciones para nuevos proyectos

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,21 +1,50 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const NuevoProyecto = () => {
+
+    // staste para Proyecto
+    const [proyecto, guardarProyecto] = useState({
+        nombre: ''
+    })
+
+    // extraer nombre de proyecto
+    const { nombre } = proyecto
+
+    const onChangeProyecto = e => {
+        guardarProyecto({
+            ...proyecto,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const onSubmitProyecto = e => {
+        e.preventDefault()
+
+        // validar el proyecto
+
+        // agregar al state
+
+        // reiniciar el form
+    }
+
     return ( 
         <Fragment>
             <button
                 type='button'
-                className='btn btn-block btn-primary'
-            ></button>
+                className='btn btn-block btn-primario'
+            >Nuevo Proyecto</button>
 
             <form 
                 className='formulario-nuevo-proyecto'
+                onSubmit={onSubmitProyecto}
             >
                 <input
                     type='text'
                     className='input-text'
                     placeholder='Nombre Proyecto'
                     name='nombre'
+                    value={nombre}
+                    onChange={onChangeProyecto}
                 />
                 <input
                     type='submit'
