df6e63052aae0f34e8443dde126f55e9658358fb
Freddy <freddy.artea@gmail.com>
2020-03-04 11:39:28 -0500

    ocultando el formulario de tareas si no hay un prpoyecto seleccionado

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext'
+
 
 const FormTarea = () => {
+
+    // extraer si un prpoyecto estas activo
+    const proyectosContext = useContext(proyectoContext)
+    const { proyecto } = proyectosContext
+    
+    // si no hay proyecTo seleccionado
+    if (!proyecto) return null
+
+    // Array destructuring para extraer el proyecto actual
+    const [proyectoActual] = proyecto
+
     return ( 
         <div className="formulario">
             <form>
