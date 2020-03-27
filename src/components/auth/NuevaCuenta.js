5be4a7785b84cc5e5d33a9d393f4beb1a24c2b20
Freddy <freddy.artea@gmail.com>
2020-03-27 10:30:31 -0500

    validando passwords

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -36,11 +36,22 @@ const NuevaCuenta = () => {
              password.trim() === '' || 
              confirmar.trim() === '' ) {
                  mostrarAlerta('Todos los campos son obligatorios', 'alerta-error')
+                 return
              }
 
         // password minimo de 6 caracteres
 
+        if ( password.length < 6 ) {
+            mostrarAlerta('El password debe ser de almenos 6 caracteres', 'alerta-error')
+            return
+        }
+
         // los 2 password sean iguales
+
+        if ( password !== confirmar ){
+            mostrarAlerta('Los passwords no son iguales', 'alerta-error')
+            return
+        }
 
         // pasarlo al action
     }
